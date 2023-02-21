246e5bb3e7bd6023133c8abc050b75c5fa9c25d3
Isabel Pereira <isabelalvaro116@gmail.com>
2023-02-21 17:21:51 +0000

    foi excluida o mobile

--- mobile/assets/main.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('btnMobileMenu')
-const nav = document.getElementById('navDisplayMobile')
-
-btn.addEventListener('click', function (event) {
-  event.preventDefault()
-
-  nav.classList.toggle('active')
-})
