c002b10545d1783898992f4a27f0b8ba1ceb0dd1
Isabel Pereira <isabelalvaro116@gmail.com>
2023-04-18 17:46:01 -0300

    added animacao

--- desafio15/assets/js/main.js
+++ desafio15/assets/js/main.js
@@ -1,13 +1,6 @@
+ScrollReveal({
+  origin:'bottom',
+  distance:'30px',
+  duration:1500,
 
-
-document.querySelector("#card_number_input").oninput = () =>{
-document.querySelector(".cc-number").innerHTML =  document.querySelector("#card_number_input").value
-}
-
-document.querySelector("#card_name").oninput = () =>{
-  document.querySelector(".label").innerHTML =  document.querySelector("#card_name").value
-  }
-
-  document.querySelector("#validate").oninput = () =>{
-    document.querySelector(".validate").innerHTML =  document.querySelector("#validate").value
-    }
+}).reveal('.cards, .bottom-content, button, p, h3')
